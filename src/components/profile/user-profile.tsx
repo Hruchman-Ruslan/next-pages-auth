86c2db37b50a4cb7804de455eebaf937e1e4c079
Ruslan <Giovingo92@gmail.com>
2024-07-26 19:04:26 +0300

    feat: adding client side protection

--- src/components/profile/user-profile.tsx
+++ src/components/profile/user-profile.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+
+import { getSession } from "next-auth/react";
+
 import ProfileForm from "./profile-form";
 
 import classes from "./user-profile.module.css";
@@ -5,6 +9,26 @@ import classes from "./user-profile.module.css";
 export interface UserProfileProps {}
 
 export default function UserProfile({}: UserProfileProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const session = await getSession();
+
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    };
+
+    checkSession();
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
