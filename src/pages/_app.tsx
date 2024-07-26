5b0b75f9f15c4283fc369a1939d2b1d04a1e2228
Ruslan <Giovingo92@gmail.com>
2024-07-26 15:53:54 +0300

    feat: active session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from "next/app";
 
+import { SessionProvider } from "next-auth/react";
+
 import "@/styles/globals.css";
 
 import Layout from "@/components/layout/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
