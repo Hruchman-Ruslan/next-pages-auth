4379876fa801e458bf359110f6680ba24796fda1
Ruslan <Giovingo92@gmail.com>
2024-07-27 15:32:40 +0300

    feat: adding change-password api route

---
+++ src/pages/api/user/change-password.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { getSession } from "next-auth/react";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "PATCH") {
+    return;
+  }
+
+  const session = await getSession({ req: req });
+
+  if (!session) {
+    res.status(401).json({ message: "Not authenticated!" });
+    return;
+  }
+}
+
+export default handler;
